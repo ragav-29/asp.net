1db70065d9a869d4070492c92630e51dcc520027
ragav-29 <141573563+ragav-29@users.noreply.github.com>
2024-10-17 23:52:46 +0530

    Add files via upload

---
+++ automationscript.py
@@ -0,0 +1,14 @@
+import os
+import subprocess
+
+
+def main():
+
+    os.chdir(os.path.join("..","..","azagent"))
+    print("changed directory to:", os.getcwd())
+
+    try:
+        subprocess.run(["cmd", "/c", "run.cmd"], check=True)
+        print("Agent Started successfully.")
+    except subprocess.CalledProcessError as e:
+        print("Error starting agent :(", e)
